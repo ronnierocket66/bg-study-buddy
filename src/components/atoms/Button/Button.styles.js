95b16d3633b37ee0a18576c2a1f0b0dfa03360fd
bartek badylewski <badylewski@gmail.com>
2021-08-25 21:27:48 +0200

    with average indicator-solution

--- src/components/atoms/Button/Button.styles.js
+++ src/components/atoms/Button/Button.styles.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-  width: 40px;
-  height: 40px;
-  background-color: #c0c7d6;
+  width: 22px;
+  height: 22px;
+  margin: 0 10px;
+  background-color: ${({ theme }) => theme.colors.grey};
   border-radius: 50px;
   border: none;
   color: white;
+  display: flex;
   justify-content: center;
   align-items: center;
-
   svg {
-    stroke: red;
     width: 100%;
     height: 100%;
   }
