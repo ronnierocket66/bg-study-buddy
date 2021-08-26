cd976924baaa353498f31ffd866464c448baeeb7
bartek badylewski <badylewski@gmail.com>
2021-08-26 14:18:28 +0200

    button eventHandler added arrow added

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import Button from 'components/atoms/Button/Button';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 
-const showIndex = (index) => {alert(`Hi there I'm student number ${index + 1}`)}
-
-const UsersListItem = ({index, userData: { average, name, attendance = '0%' } }) => (
-  <Wrapper>
-    <StyledAverage value={average}>{average}</StyledAverage>
-    <StyledInfo>
-      <p>
-        {name}
-        <Button onMouseEnter={() => showIndex(index)} />
-      </p>
-      <p>attendance: {attendance}</p>
-    </StyledInfo>
-  </Wrapper>
-);
+const UsersListItem = ({ index, userData: { average, name, attendance = '0%' } }) => {
+  return (
+    <Wrapper>
+      <StyledAverage value={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>
+          {name}
+          <Button onMouseEnter={() => alert(`This is student #${index + 1}`)} />
+        </p>
+        <p>attendance: {attendance}</p>
+      </StyledInfo>
+    </Wrapper>
+  );
+};
 
 UsersListItem.propTypes = {
   userData: PropTypes.shape({
