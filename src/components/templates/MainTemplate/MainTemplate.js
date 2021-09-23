a84f3ad59f684d59c3f95ee34bceb06a74802431
bartek badylewski <badylewski@gmail.com>
2021-09-23 13:23:41 +0200

    initial commit

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navigation from 'components/organisms/Navigation/Navigation';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
+
+const SearchBar = styled.div`
+  grid-row: 1 / 2 ;
+  grid-column: 2 / 3;
+  border-bottom: 1px solid ${({theme}) => theme.colors.darkPurple};
+`;
+
+const News = styled.div`
+  grid-row: 1 / 3;
+  grid-column: 3 / 3;
+  border-left: 1px solid ${({theme}) => theme.colors.darkPurple};
+`;
 
 const MainTemplate = ({ children }) => {
   return (
     <Wrapper>
       <Navigation />
+      <SearchBar />
       {children}
+      <News>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+      </News>
     </Wrapper>
   );
 };
