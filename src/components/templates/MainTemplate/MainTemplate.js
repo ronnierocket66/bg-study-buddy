250f6d1a206c7c5d978a71885214dd91adeb622f
bartek badylewski <badylewski@gmail.com>
2021-09-28 17:56:39 +0200

    working add new template organism

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import Navigation from 'components/organisms/Navigation/Navigation';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
-
-const SearchBar = styled.div`
-  grid-row: 1 / 2 ;
-  grid-column: 2 / 3;
-  border-bottom: 1px solid ${({theme}) => theme.colors.darkPurple};
-`;
-
-const News = styled.div`
-  grid-row: 1 / 3;
-  grid-column: 3 / 3;
-  border-left: 1px solid ${({theme}) => theme.colors.darkPurple};
-`;
+import { SearchBar } from 'components/organisms/SearchBar/SearchBar';
+import NewsSection from 'components/templates/NewsSection/NewsSection';
 
 const MainTemplate = ({ children }) => {
   return (
@@ -21,14 +10,7 @@ const MainTemplate = ({ children }) => {
       <Navigation />
       <SearchBar />
       {children}
-      <News>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-      </News>
+      <NewsSection />
     </Wrapper>
   );
 };
